5dcf7329d8d2e5f47d742b69cd16f761b7c226fd
Matheus Pratta <matpratta@gmail.com>
2021-01-11 23:18:43 -0300

    Initial version :D

---
+++ README.md
@@ -0,0 +1,51 @@
+# Lua Compiler for Dual Universe
+
+This is a very simple compiler for Dual Universe scripts, specially larger projects.
+
+It does some pre-processing on your scripts, such as embedding `require` statements and code minification.
+
+As an bonus, it also has some very basic project management through a `project.json` file (check below)
+
+## Installation
+
+To install this compiler, just run `npm install -g @wolfe-labs/du-luac` and it will be installed to your CLI
+
+## Usage
+
+To compile your projects, use the following syntax: `du-luac [path-to/project.json]`
+
+You might need to generate a `project.json` file first, it must have the following syntax:
+
+```json
+{
+  "entryPoint": "src/main",
+  "handleErrors": true,
+  "slots": {
+    "screen": {
+      "type": "screen"
+    },
+    "db": {
+      "type": "databank"
+    }
+  },
+  "output": {
+    "path": "out/main",
+    "complete": true,
+    "minify": true,
+    "minifyHeaders": [
+      "Your project name here",
+      "Another line of comment on the minified file"
+    ]
+  }
+}
+```
+
+The `entryPoint` field is mandatory and must point to a file inside the project directory.
+
+The `handleErrors` field will enable the `--handle-errors` flag for the autoconfig generator.
+
+The `slots` field allows you to specify each of your construct's slots and their name on the autoconfig file, it **must** follow the sequence you linked your components.
+
+The `output` field allows you to customize output settings, like if the entire `complete` output will be needed (good for debugging!) and if you will need a `minify` output too, for smaller code payloads.
+
+The `minifyHeaders` sub-field allows you to specify lines of comments that will be added before the minified file code (if you want to add a project name or license there).
---
+++ build.js
@@ -0,0 +1,191 @@
+#!/usr/bin/env node
+
+/**
+ * Simple Lua processor for combining and minifying Lua code for distribution
+ */
+
+const fs = require('fs')
+const path = require('path')
+const luamin = require('luamin')
+const luaparse = require('luaparse')
+const { spawnSync } = require('child_process')
+
+const srcpath = []
+
+function handleRequire (file) {
+  // Process the file name
+  const filename = handleFilename(file)
+
+  // File not found?
+  if (!filename) {
+    console.error(`File not found: ${file}`)
+    process.exit(1)
+  }
+
+  // Setup current path
+  srcpath.unshift(path.dirname(file))
+
+  // Gets the file contents
+  const source = fs.readFileSync(filename).toString()
+
+  // Process the file source and return it
+  const result = handleSource(source)
+
+  // Removes from path to keep working
+  srcpath.shift()
+
+  // Returns processed source
+  return result
+}
+
+function handleFilename (file) {
+  // Get absolute path
+  file = path.join(srcpath[0], file)
+
+  // Works on file
+  if (fs.existsSync(file)) return file
+  if (fs.existsSync(file + '.lua')) return file + '.lua'
+  return null
+}
+
+function handleSource (source) {
+  // All regexes go here
+  const regex = {
+    require: {
+      expression: /require[\s]+[\'\"](.+?)[\'\"]/gi,
+      handle: function (match, file) {
+        return handleRequire(file)
+      },
+    },
+  }
+
+  // Executes each regex
+  for (let key in regex) {
+    source = source.replace(
+      regex[key].expression,
+      regex[key].handle
+    );
+  }
+
+  // Returns processed source
+  return source
+}
+
+// Push CWD into the compile directory
+srcpath.unshift(process.cwd())
+
+// The project JSON file
+const projectSource = process.argv[2] || path.join(srcpath[0], 'build.json')
+
+// Logs
+console.info(`Loading project file: ${projectSource}`)
+
+// Check if project exists
+if (!fs.existsSync(projectSource)) {
+  console.error(`Project file [${projectSource}] was not found.`)
+  process.exit(1)
+}
+
+// Read project file
+const project = JSON.parse(fs.readFileSync(projectSource).toString())
+
+// Get entry point
+const fileInput = process.argv[2] || 'src/main'
+const fileOutput = process.argv[3] || 'out/main'
+
+// Logs
+console.info(`Fetching project entry point: ${project.entryPoint}`)
+
+// Load first file and process it
+let output = handleRequire(project.entryPoint)
+
+// Logs
+console.info(`Building LUA AST and checking for syntax errors...`)
+
+// Generate the AST for error checking
+let outputAST
+try {
+  outputAST = luaparse.parse(output)
+} catch (e) {
+  console.error('Error while parsing LUA file:')
+  console.error(e)
+  process.exit(1)
+}
+
+// Generate autoconf slots
+const slots = Object.keys(project.slots || {}).map(slot => {
+  // Get slot metadata
+  const meta = project.slots[slot]
+
+  // Generate string
+  let slotString = slot
+
+  // Include type, if possible
+  if (meta.type) slotString += `:type=${meta.type}`
+
+  // Done, returns the slot string
+  return slotString
+})
+
+// Create output dir if needed
+if (!fs.existsSync(path.dirname(project.output.path))) {
+  fs.mkdirSync(path.dirname(project.output.path))
+}
+
+// Generate build arguments
+const buildProg = path.join(__dirname, 'wrap.lua')
+const buildArgs = [
+  (project.handleErrors) ? '--handle-errors' : '',
+
+  (slots.length > 0) ? '--slots' : '',
+  ...slots,
+]
+
+// Build (normal)
+if (project.output.complete) {
+  // File output
+  const file = path.resolve(project.output.path)
+
+  // Output some logs
+  console.info(`Preparing Lua output: ${file}`)
+
+  // Write Lua files
+  fs.writeFileSync(`${file}.lua`, output)
+
+  // More logs
+  console.info(`Generating autoconf file with following flags: ${buildArgs.join(' ')}`)
+
+  // Generate autoconf
+  const outputAutoconf = spawnSync('lua', [buildProg, `${file}.lua`, `${file}.json`, ...buildArgs]).output.join('\n').trim()
+  
+  // Shows any outputs
+  if (outputAutoconf.length > 0) {
+    console.log(outputAutoconf)
+  }
+}
+
+// Build (minified)
+if (project.output.minify) {
+  // File output
+  const file = path.resolve(project.output.path) + '.min'
+
+  // Output some logs
+  console.info(`Preparing Lua output: ${file}`)
+
+  // Write Lua files
+  fs.writeFileSync(`${file}.lua`, `-- ${(project.output.minifyHeaders || []).join("\n-- ")}\n${luamin.minify(outputAST)}`)
+
+  // More logs
+  console.info(`Generating autoconf file with following flags: ${buildArgs.join(' ')}`)
+
+  // Generate autoconf
+  const outputAutoconf = spawnSync('lua', [buildProg, `${file}.lua`, `${file}.json`, ...buildArgs]).output.join('\n').trim()
+  
+  // Shows any outputs
+  if (outputAutoconf.length > 0) {
+    console.log(outputAutoconf)
+  }
+}
+
+// Yay!
+console.info('Done.')
