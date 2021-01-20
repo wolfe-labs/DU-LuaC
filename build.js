f9565393d21c1f6f0f9c6ab96b732dee27821a49
Matheus Pratta <matpratta@gmail.com>
2021-01-19 23:37:36 -0300

    Interactive CLI; External (Git) library support; Documentation; Version bump to 0.4.0;

--- build.js
+++ build.js
@@ -1,195 +1,200 @@
-#!/usr/bin/env node
-
 /**
  * Simple Lua processor for combining and minifying Lua code for distribution
  */
 
-const fs = require('fs')
-const path = require('path')
+module.exports = async function (argv) {
 
-// The compiler
-const compile = require('./compiler')
+  const fs = require('fs')
+  const path = require('path')
 
-// The minifier
-const minify = require('luamin').minify
+  // The compiler
+  const compile = require('./compiler')
 
-// The currently loaded libs
-const libraries = {}
+  // The minifier
+  const minify = require('luamin').minify
 
-// The thing that boots up wrap.lua
-const { spawnSync } = require('child_process')
+  // The currently loaded libs
+  const libraries = {}
 
-// The default project file
-const defaultBuildFile = 'project.json'
-    
-// Our wrap.lua file
-const buildProg = path.join(__dirname, 'wrap.lua')
+  // The thing that boots up wrap.lua
+  const { spawnSync } = require('child_process')
 
-// Gets current Lua version
-function getLuaVersion () {
-  const luaVersion = spawnSync('lua', ['-v'])
-  if (luaVersion && luaVersion.stdout) {
-    return luaVersion.stdout.toString()
-  } else {
-    return null
-  }
-}
-
-// Load library
-function loadLibrary (library) {
-  // Checks if the "id" field is present
-  if (!library.id) {
-    console.error('Invalid library entry, missing "id" field:', library)
-    process.exit(1)
-  }
-
-  // If library is already loaded, skip loading it
-  if (libraries[library.id]) return
-
-  // TODO: Handle library downloads
-
-  // Looks for the library file
-  library.path = path.resolve(library.path)
-
-  // Check if it exists
-  if (!fs.existsSync(library.path)) {
-    console.error(`Library path not found for library "${library.id}": ${library.path}`)
-    process.exit(1)
-  }
-
-  // Check if the project file also exists
-  const libProject = path.join(library.path, defaultBuildFile)
-  if (!fs.existsSync(libProject)) {
-    console.error(`Project file missing for library "${library.id}": ${libProject}`)
-    process.exit(1)
-  }
-
-  // Log
-  console.info(`Loading project library: "${library.id}"`)
-
-  // Fetch the library project
-  library.project = JSON.parse(fs.readFileSync(libProject).toString())
-
-  // Resolve the library root
-  library.root = path.dirname(path.join(library.path, library.project.sourcePath || 'src'))
-
-  // Add to libraries
-  libraries[library.id] = library
-}
-
-// Load package info
-const cPackage = require(path.join(__dirname, 'package.json'))
-
-// Welcome msg :)
-console.info(`DU-LuaC compiler v${cPackage.version} @ env. ${process.version}`)
-
-// Lua info
-const luaVersion = getLuaVersion()
-if (luaVersion) {
-  console.info(luaVersion)
-} else {
-  console.warn('No Lua tools found on your system. Autoconfig might not be generated.')
-  console.warn('You can download the binaries at: http://luabinaries.sourceforge.net/')
-}
-
-// Skip line
-console.info('')
-
-// The project JSON file
-const projectSource = process.argv[2] || path.join(process.cwd(), defaultBuildFile)
-
-// Logs
-console.info(`Loading project file: ${projectSource}`)
-
-// Check if project exists
-if (!fs.existsSync(projectSource)) {
-  console.error(`Project file [${projectSource}] was not found.`)
-  process.exit(1)
-}
-
-// Read project file
-const project = JSON.parse(fs.readFileSync(projectSource).toString())
-
-// Load current project as library
-loadLibrary({ id: project.name, path: process.cwd() })
-
-// Read libraries
-if (project.libs) {
-  project.libs.map(loadLibrary)
-}
-
-// Skip line
-console.info('')
-
-// Runs builds
-;(project.builds || []).map(buildSpec => {
-  // Info
-  console.info(`Compiling build "${buildSpec.name}"...`)
-
-  // Does the compile step
-  const result = compile(project, buildSpec.name, libraries)
-
-  // Gets the slots for autoconf
-  const slots = Object.keys(buildSpec.slots || {}).map(slot => {
-    // Get slot metadata
-    const meta = buildSpec.slots[slot]
-
-    // Generate string
-    let slotString = slot
-
-    // Include type, if possible
-    if (meta.type) slotString += `:type=${meta.type}`
-
-    // Done, returns the slot string
-    return slotString
-  })
-
-  // Runs the build targets
-  ;(project.targets || []).map(buildTarget => {
-    // Info
-    console.info(`Generating build files for target "${buildTarget.name}"...`)
-
-    // Get a clean copy of the compile output
-    let output = result.output
-
-    // Minify?
-    if (buildTarget.minify) output = minify(result.outputAST)
-
-    // The base filename
-    const file = path.join(process.cwd(), project.outputPath, buildTarget.name, buildSpec.name)
-
-    // Generate build arguments
-    const buildArgs = [
-      // Add error-handling code?
-      (buildTarget.handleErrors) ? '--handle-errors' : null,
-
-      // Add slots for auto-mapping
-      (slots.length > 0) ? '--slots' : null,
-      ...slots,
-    ].filter(arg => !!arg)
-
-    // Create output dirs if needed
-    if (!fs.existsSync(path.dirname(file))) {
-      fs.mkdirSync(path.dirname(file), { recursive: true })
-    }
-
-    // Write Lua files
-    fs.writeFileSync(`${file}.lua`, output)
-
-    if (luaVersion) {
-      // More logs
-      console.info(`Generating autoconf file with following flags: ${buildArgs.join(' ')}`)
-
-      // Generate autoconf
-      const procAutoconf = spawnSync('lua', [buildProg, `${file}.lua`, `${file}.json`, ...buildArgs])
-      const outputAutoconf = (procAutoconf.output || []).join('\n').trim()
+  // The default project file
+  const defaultBuildFile = 'project.json'
       
-      // Shows any outputs
-      if (outputAutoconf.length > 0) {
-        console.info(outputAutoconf)
-      }
+  // Our wrap.lua file
+  const buildProg = path.join(__dirname, 'wrap.lua')
+
+  // Gets current Lua version
+  function getLuaVersion () {
+    const luaVersion = spawnSync('lua', ['-v'])
+    if (luaVersion && luaVersion.stdout) {
+      return luaVersion.stdout.toString()
     } else {
-      console.info('Skipping autoconf file generation as no Lua tools were found')
+      return null
     }
+  }
+
+  // Load library
+  async function loadLibrary (library) {
+    // Checks if the "id" field is present
+    if (!library.id) {
+      console.error('Invalid library entry, missing "id" field:', library)
+      process.exit(1)
+    }
+
+    // If library is already loaded, skip loading it
+    if (libraries[library.id]) return
+
+    // Check if the library is still pending download
+    if (library.remote && library.remote.git && !fs.existsSync(library.path)) {
+      // Tell user
+      console.info(`Fetching remote library "${library.id}"...`)
+
+      // Fetches library
+      await require('./library').loadExternalLibrary(library.remote.git, 'libs', library.path)
+    }
+
+    // Looks for the library file
+    library.path = path.resolve(library.path)
+
+    // Check if it exists
+    if (!fs.existsSync(library.path)) {
+      console.error(`Library path not found for library "${library.id}": ${library.path}`)
+      process.exit(1)
+    }
+
+    // Check if the project file also exists
+    const libProject = path.join(library.path, defaultBuildFile)
+    if (!fs.existsSync(libProject)) {
+      console.error(`Project file missing for library "${library.id}": ${libProject}`)
+      process.exit(1)
+    }
+
+    // Log
+    console.info(`Loading project library: "${library.id}"`)
+
+    // Fetch the library project
+    library.project = JSON.parse(fs.readFileSync(libProject).toString())
+
+    // Resolve the library root
+    library.root = path.dirname(path.join(library.path, library.project.sourcePath || 'src'))
+
+    // Add to libraries
+    libraries[library.id] = library
+  }
+
+  // Lua info
+  const luaVersion = getLuaVersion()
+  if (luaVersion) {
+    console.info(luaVersion)
+  } else {
+    console.warn('No Lua tools found on your system. Autoconfig might not be generated.')
+    console.warn('You can download the binaries at: http://luabinaries.sourceforge.net/')
+  }
+
+  // Skip line
+  console.info('')
+
+  // The project JSON file
+  const projectSource = argv[0] || path.join(process.cwd(), defaultBuildFile)
+
+  // Logs
+  console.info(`Loading project file: ${projectSource}`)
+
+  // Check if project exists
+  if (!fs.existsSync(projectSource)) {
+    console.error(`Project file [${projectSource}] was not found.`)
+    process.exit(1)
+  }
+
+  // Read project file
+  const project = JSON.parse(fs.readFileSync(projectSource).toString())
+
+  // Load current project as library
+  await loadLibrary({ id: project.name, path: process.cwd() })
+
+  // Read libraries
+  if (project.libs) {
+    for (let k in project.libs) {
+      await loadLibrary(project.libs[k])
+    }
+  }
+
+  // Skip line
+  console.info('')
+
+  // Runs builds
+  ;('object' == typeof project.builds ? Object.values(project.builds) : project.builds || []).map(buildSpec => {
+    // Info
+    console.info(`Compiling build "${buildSpec.name}"...`)
+
+    // Does the compile step
+    const result = compile(project, buildSpec.name, libraries)
+
+    // Gets the slots for autoconf
+    const slots = Object.keys(buildSpec.slots || {}).map(slot => {
+      // Get slot metadata
+      const meta = buildSpec.slots[slot]
+
+      // Generate string
+      let slotString = slot
+
+      // Include type, if possible
+      if (meta.type) slotString += `:type=${meta.type}`
+
+      // Done, returns the slot string
+      return slotString
+    })
+
+    // Runs the build targets
+    ;('object' == typeof project.targets ? Object.values(project.targets) : project.targets || []).map(buildTarget => {
+      // Info
+      console.info(`Generating build files for target "${buildTarget.name}"...`)
+
+      // Get a clean copy of the compile output
+      let output = result.output
+
+      // Minify?
+      if (buildTarget.minify) output = minify(result.outputAST)
+
+      // The base filename
+      const file = path.join(process.cwd(), project.outputPath, buildTarget.name, buildSpec.name)
+
+      // Generate build arguments
+      const buildArgs = [
+        // Add error-handling code?
+        (buildTarget.handleErrors) ? '--handle-errors' : null,
+
+        // Add slots for auto-mapping
+        (slots.length > 0) ? '--slots' : null,
+        ...slots,
+      ].filter(arg => !!arg)
+
+      // Create output dirs if needed
+      if (!fs.existsSync(path.dirname(file))) {
+        fs.mkdirSync(path.dirname(file), { recursive: true })
+      }
+
+      // Write Lua files
+      fs.writeFileSync(`${file}.lua`, output)
+
+      if (luaVersion) {
+        // More logs
+        console.info(`Generating autoconf file with following flags: ${buildArgs.join(' ')}`)
+
+        // Generate autoconf
+        const procAutoconf = spawnSync('lua', [buildProg, `${file}.lua`, `${file}.json`, ...buildArgs])
+        const outputAutoconf = (procAutoconf.output || []).join('\n').trim()
+        
+        // Shows any outputs
+        if (outputAutoconf.length > 0) {
+          console.info(outputAutoconf)
+        }
+      } else {
+        console.info('Skipping autoconf file generation as no Lua tools were found')
+      }
+    })
   })
-})
+
+}
---
+++ library.js
@@ -0,0 +1,179 @@
+const fs = require('fs').promises
+const path = require('path')  
+const exists = require('fs').existsSync
+
+const Git = require('nodegit')
+const GitUrlParse = require('git-url-parse')
+
+const crypto = require('crypto')
+const sshpk = require('sshpk')
+const sha1 = require('sha1')
+  
+module.exports = class Library {
+
+  static async saveProject (project, path) {
+    return await fs.writeFile(path || 'project.json', JSON.stringify(project, null, '  '))
+  }
+
+  static projectHasLibrary (project, library) {
+    for (let k in project.libs || []) {
+      if (project.libs[k].id == library) return true
+    }
+    return false
+  }
+
+  static projectHasBuild (project, build) {
+    return !!project.builds[build]
+  }
+
+  static projectHasTarget (project, target) {
+    return !!project.targets[target]
+  }
+
+  static async getKeys () {
+    const pathKeys = path.join(__dirname, 'keys')
+    const pathKPublic = path.join(pathKeys, 'id.pub')
+    const pathKPrivate = path.join(pathKeys, 'id')
+
+    // Checksi f key directory exits
+    if (!exists(pathKeys)) {
+      fs.mkdir(pathKeys, { recursive: true })
+    }
+
+    // Checks if keys exist, if not then generates them
+    if (!(exists(pathKPrivate) && exists(pathKPublic))) {
+      // Generates keys
+      const keysGen = crypto.generateKeyPairSync('ed25519', {
+        publicKeyEncoding: {
+          type: 'spki',
+          format: 'pem',
+        },
+        privateKeyEncoding: {
+          type: 'pkcs8',
+          format: 'pem',
+        },
+      })
+
+      // Writes them to disk
+      fs.writeFile(pathKPublic, keysGen.publicKey)
+      fs.writeFile(pathKPrivate, keysGen.privateKey)
+    }
+
+    // Fetches both keys in original format
+    const keys = {
+      public: await fs.readFile(pathKPublic),
+      private: await fs.readFile(pathKPrivate),
+    }
+
+    // Converts them to SSH format
+    keys.public_ssh = sshpk.parseKey(keys.public).toString('ssh')
+    keys.private_ssh = sshpk.parsePrivateKey(keys.private).toString('ssh')
+
+    // Returns keys
+    return keys
+  }
+
+  static async loadExternalLibrary (gitUrl, libDir, destDir) {
+    // Tries to parse Git URL
+    const gitInfo = GitUrlParse(gitUrl)
+
+    // Destination or default to temp
+    const destination = destDir || path.join(__dirname, 'temp/' + sha1(gitUrl))
+
+    // If no destination set and temp exists, remove it
+    if (!destDir && exists(destination)) {
+      await fs.rmdir(destination, { recursive: true })
+    }
+
+    // Validates, protocol MUST NOT be 'file' as it may point to an invalid local path
+    if (gitInfo && gitInfo.protocol && 'file' != gitInfo.protocol) {
+      // Logs information
+      console.info(`Downloading library files from: "${gitUrl}"`)
+
+      // The clone result will go here
+      let cloneResult = null
+
+      // Tries to download library via supplied URL
+      try {
+        cloneResult = await Git.Clone(gitUrl, destination)
+      } catch (e) {
+        // Tries to download library via SSH
+        try {
+          // Gets the SSH Git URL
+          const gitSSH = `git@${gitInfo.resource}:${gitInfo.full_name}.git`
+
+          // Loads SSH private keys
+          const sshKeys = await Library.getKeys()
+
+          // Attempts clone again
+          cloneResult = await Git.Clone(gitSSH, destination, {
+            fetchOpts: {
+              callbacks: {
+                // Disable certificate check as this may result in error
+                certificateCheck: () => 0,
+
+                // Allows for SSH auth
+                credentials: (url, user) => {
+                  // Returns DU-LuaC's generated keys
+                  return Git.Cred.sshKeyMemoryNew(user, sshKeys.public_ssh, sshKeys.private_ssh, '')
+                },
+              },
+            },
+          })
+        } catch (e) {
+          console.error(`Error while fetching repository from Git:`, e)
+          console.error(`Make sure you have the application's SSH public key authorized on your Git provider. To find it, run "du-lua git-key"`)
+          console.error(`Also, make sure the following Git path is correct:`, gitUrl)
+          process.exit(1)
+        }
+      }
+
+      // If no results are found, fail
+      if (!cloneResult) return null
+
+      // Parses the library info!
+      const libInfo = await Library.getProjectInfo(destination)
+
+      // Only moves if no destination is provided!
+      if (!destDir) {
+        // Gets names
+        const destName = path.join(libDir, libInfo.name)
+
+        // Removes existing library if existing
+        if (exists(destName)) {
+          await fs.rmdir(destName, { recursive: true })
+        }
+
+        // Creates destination dir if not existing
+        if (!exists(destName)) {
+          await fs.mkdir(destName, { recursive: true })
+        }
+
+        // Does the move
+        await fs.rename(destination, destName)
+
+        // Adds a proper path to it
+        libInfo.path = destName
+      }
+
+      // Returns library info
+      return libInfo
+    } else {
+      // Fails
+      return null
+    }
+  }
+
+  // Helper to get current project
+  static async getProjectInfo (directory) {
+    // Checks for existing project file
+    if (!exists(path.join(directory, 'project.json'))) {
+      console.error(`Project file not found "${path.join(directory, project.json)}"! Please, run "du-lua init" first!`)
+      process.exit(1)
+    }
+
+    // Loads project
+    return JSON.parse((await fs.readFile(path.join(directory, 'project.json'))).toString())
+  }
+
+}
