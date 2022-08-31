bba4b330254a5e29b61325b01ab98e25977f0bdb
Matheus Pratta <matpratta@gmail.com>
2022-08-30 22:51:33 -0300

    Better integration and descriptions of AutoConfig

--- Codex/CodexExtras.lua
+++ Codex/CodexExtras.lua
@@ -1,18 +1,18 @@
---- Returns a list of links of current unit
+--- Returns a list of elements linked to the current Control Unit
 ---@param filters table A list of filters where each key is a element function and the value is the desired value
 ---@param noLinkNames boolean When true the results will be keyed by the linking order instead of link name
----@return table
+---@return table<string|number, Element>
 function library.getLinks(filters, noLinkNames) end
 
---- Returns a list of links of current unit by class
+--- Returns a list of elements linked to the current Control Unit of a specified class
 ---@param className string The element class name you want to match against
 ---@param noLinkNames boolean When true the results will be keyed by the linking order instead of link name
----@return table
+---@return table<string|number, Element>
 function library.getLinksByClass(className, noLinkNames) end
 
---- Returns a list of links of current unit by class
+--- Returns the first element linked to the current Control Unit of a specified class
 ---@param className string The element class name you want to match against
----@return table
+---@return Element
 function library.getLinkByClass(className) end
 
 --- Returns the linked Core Unit
--- lua/AutoConfig.lua
+++ lua/AutoConfig.lua
@@ -1,10 +1,10 @@
 ---@class Library
 local library = DULibrary
 
---- Returns a list of links of current unit
+--- Returns a list of elements linked to the current Control Unit
 ---@param filters table A list of filters where each key is a element function and the value is the desired value
 ---@param noLinkNames boolean When true the results will be keyed by the linking order instead of link name
----@return table
+---@return table<string|number, Element>
 function library.getLinks(filters, noLinkNames)
   -- List of links found
   local links = {}
@@ -47,19 +47,19 @@ function library.getLinks(filters, noLinkNames)
   return links
 end
 
---- Returns a list of links of current unit by class
+--- Returns a list of elements linked to the current Control Unit of a specified class
 ---@param className string The element class name you want to match against
 ---@param noLinkNames boolean When true the results will be keyed by the linking order instead of link name
----@return table
+---@return table<string|number, Element>
 function library.getLinksByClass(className, noLinkNames)
   return library.getLinks({
     getClass = className,
   }, noLinkNames)
 end
 
---- Returns a list of links of current unit by class
+--- Returns the first element linked to the current Control Unit of a specified class
 ---@param className string The element class name you want to match against
----@return table
+---@return Element
 function library.getLinkByClass(className)
   return library.getLinksByClass(className, true)[1]
 end
