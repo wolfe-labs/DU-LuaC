5c887be5afec55da54ca73d943c2a4e42b2f1f93
Matheus Pratta <matpratta@gmail.com>
2022-09-19 21:34:44 -0300

    Events now support passing callable objects and threads as handlers

--- lua/Events.lua
+++ lua/Events.lua
@@ -1,13 +1,15 @@
---- Adds event handling to an object's instance
----@param obj T The object you are adding events to
+-- Adds event handling to an object's instance
 function library.addEventHandlers(obj)
   -- Does nothing when obj is null or already has events
   if (not obj) or (obj.onEvent and obj.triggerEvent) then
     return false
   end
 
+  -- Some types, this will be minified and used later
+  local T_TABLE, T_FUNCTION, T_THREAD = 'table', 'function', 'thread'
+
   -- Makes sure we're applying it to a table
-  if 'table' ~= type(obj) then
+  if T_TABLE ~= type(obj) then
     error('You can only add event handlers to table objects!')
   end
 
@@ -17,16 +19,33 @@ function library.addEventHandlers(obj)
   -- This will store the event handlers
   local eventHandlers = {}
 
+  -- Asserts something is callable
+  local function isCallable(obj)
+    return (
+      (T_FUNCTION == type(obj))
+      or (T_TABLE == type(obj) and isCallable(getmetatable(obj).__call))
+      or (T_THREAD == type(obj) and 'dead' ~= coroutine.status(obj))
+    )
+  end
+
+  -- Invokes a callable object
+  local function invoke(obj, ...)
+    return (
+      ((T_FUNCTION == type(obj) or (T_TABLE == type(obj) and isCallable(getmetatable(obj).__call))) and obj(...))
+      or ((T_THREAD == type(obj)) and coroutine.resume(obj, ...))
+    )
+  end
+
   -- Adds event handler
   obj.onEvent = (function (self, event, handler, selfRef)
-    -- If event handler is not a function trigger error
-    if 'function' ~= type(handler) then
+    -- If event handler is not callable trigger error
+    if not isCallable(handler) then
       error('Event handler must be a function!')
     end
 
     -- Self reference passed to the event
     selfRef = selfRef or self
-    if 'table' ~= type(selfRef) then
+    if T_TABLE ~= type(selfRef) then
       error('The value of self must be a table!')
     end
 
@@ -54,7 +73,7 @@ function library.addEventHandlers(obj)
   obj.triggerEvent = (function (self, event, ...)
     -- Processes each of the handlers
     for _, handler in pairs(eventHandlers[event] or {}) do
-      handler[1](handler[2], ...)
+      invoke(handler[1], handler[2], ...)
     end
   end)
 
